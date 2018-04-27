2d2a56d726253b400a02bf8ebaf0d4cc293d20ea
David <david.breier@outlook.de>
2018-04-27 23:31:32 +0200

    app.js angelegt und erste Logik eingebaut
    erste Logik in JS gebaut: span Elemente zeigen jetzt Platzhalter an, Submit Btn hat Listener und gibt Eingabe auf die Konsole aus. Funktion prüft ob Eingabe im Zahlenbereich liegt und gibt sonst Warnung aus. Bisher alles noch "hard coded".

---
+++ app.js
@@ -0,0 +1,35 @@
+//Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+//UI Elements
+let game = document.getElementById('game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.getElementById('guess-btn'),
+    guessInput = document.getElementById('guess-input'),
+    message = document.querySelector('.message');
+
+//Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener('click',function(){
+
+    let guess = parseInt(guessInput.value);
+    console.log(guess);
+
+    //Validation
+    if(isNaN(guess)||guess<min||guess>max){
+        setMessage(`Please Enter a Number between ${min} and ${max}`,'red');
+
+    }
+});
+
+function setMessage(msg,color){
+    message.style.color=color;
+    message.textContent=msg;
+
+}
